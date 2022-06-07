62869c6ddbcebad1b5deb08dbbd5326e7a26d7d2
Candace Robbins <canrob0522@gmail.com>
2022-06-07 09:07:39 -0500

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,16 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+// import { setContext } from '@apollo/client/link/context';
 import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Header from './components/Header';
+// import Signup from './pages/Signup';
+// import Login from './pages/Login';
+// import Header from './components/Header';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Renterportal from './components/Renterportal';
+import Ownerportal from './components/Ownerportal';
+import './styles/app.css'
 
 const httpLink = createHttpLink({
   uri: '/graphql'
@@ -40,15 +44,19 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div className="flex-column justify-flex-start min-100-vh">
-          <Header />
+          <div>
+            <Navbar />
+          </div>
           <div className="container">
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
+              <Route path="/Renterportal" element={<Renterportal />} />
+              <Route path="/Ownerportal" element={<Ownerportal />} />
             </Routes>
           </div>
-          <Footer />
+          <div>
+            <Footer />
+          </div>
         </div>
       </Router>
     </ApolloProvider>
