aff8e2cc88ee6d324b2b08f6289c3c6c89641608
Akuru <anjalismith0529@gmail.com>
2022-06-08 12:45:14 -0400

    reduced footer padding to 0

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,7 +5,7 @@ const Footer = () => {
   const location = useLocation();
   const navigate = useNavigate();
   return (
-    <footer className="w-100 fixed-bottom mt-auto text-dark p-4">
+    <footer className="w-100 fixed-bottom mt-auto text-dark p-0">
       <div className="container text-center mb-5">
         {location.pathname !== '/' && (
           <button className="btn btn-dark mb-3" onClick={() => navigate(-1)}>
