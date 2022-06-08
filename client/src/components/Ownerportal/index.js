77606b50f9919de6cca44e09b6698fdc88a40574
Candace Robbins <canrob0522@gmail.com>
2022-06-08 18:38:30 -0500

    updated ownerportal

--- client/src/components/Ownerportal/index.js
+++ client/src/components/Ownerportal/index.js
@@ -1,31 +1,47 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import PropertyCard from "../Properties";
-import { Jumbotron, Container, Button } from 'react-bootstrap';
+import { Container, CardGroup, Button } from 'react-bootstrap';
 import { useQuery } from "@apollo/client";
 import { QUERY_USER } from "../../utils/queries";
+import { QUERY_ME } from "../../utils/queries";
 // import Panel from 'react-bootstrap/Panel';
 import "../../styles/app.css";
 
 
 const Ownerportal = () => {
-  // const {data} = useQuery(QUERY_USER);
-  // let user;
-  // if (data) {
-  //   user= data.user;
-  // }
+  
+  const { loading, data} = useQuery(QUERY_ME)
+   
+  const userData = data?.me || [];
 
   return (
     <div>
-      <div fluid className="m-3">
-          <h1>Hello, George</h1>
-            <p>
-               Listed below is your rental properties.
-            </p>
-      </div>
-      <div>
-        <PropertyCard />
-      </div>
+      <Container>
+        <div fluid className="m-3">
+          <h1>Hello, {userData.contact.firstName}</h1>
+          <p>
+            <Button bsStyle="primary">Edit Contact Info</Button>
+          </p>
+        </div>
+      </Container>
+
+      <Container>
+        <h2> Properties Listed
+          {userData.property.length
+            ? `Viewing ${userData.property.length} saved ${userData.property.length === 1 ? 'property' : 'property'}:`
+            : 'You have no properties listed!'}
+        </h2>
+        <CardGroup className="flex-row">
+          {userData.property.map((property) => {
+            return (
+             <PropertyCard />
+            );
+          })}
+
+        </CardGroup>   
+
+      </Container>
     </div>
   );
 };
