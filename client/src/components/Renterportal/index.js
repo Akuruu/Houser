d0434086ec44c370956d01e74263d5f88885d73e
Hailey Bates <hailey.bates001@gmail.com>
2022-06-08 22:20:19 -0400

    Adding const for setshow modal

--- client/src/components/Renterportal/index.js
+++ client/src/components/Renterportal/index.js
@@ -11,6 +11,12 @@ const Renterportal = () => {
 
   const useData = data?.me || [];
 
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
       <Container>
