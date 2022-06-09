a7217b602036962570ecb6d314fc49ecfb1e4841
Hailey Bates <hailey.bates001@gmail.com>
2022-06-08 22:23:28 -0400

    Adding rest of forms

--- client/src/components/Renterportal/index.js
+++ client/src/components/Renterportal/index.js
@@ -83,7 +83,48 @@ const Renterportal = () => {
                 autoFocus
               />
             </Form.Group>
-            
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput4">
+              <Form.Label>City</Form.Label>
+              <Form.Control
+                type="city"
+                placeholder="Orlando"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput5">
+              <Form.Label>State</Form.Label>
+              <Form.Control
+                type="state"
+                placeholder="Florida"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput6">
+              <Form.Label>Zipcode</Form.Label>
+              <Form.Control
+                type="zipcode"
+                placeholder="12345"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput7">
+              <Form.Label>Primary Phone Number</Form.Label>
+              <Form.Control
+                type="phone1"
+                placeholder="(555-555-1234)"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput7">
+              <Form.Label>Secondary Phone Number</Form.Label>
+              <Form.Control
+                type="phone2"
+                placeholder="(555-555-4321)"
+                autoFocus
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
 
     </>
 )
